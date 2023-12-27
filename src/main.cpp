76423f6869e488b0e6dba5db7c0e85fccec180dc
TechStudent10 <techstudent10yt@gmail.com>
2023-12-27 12:45:11 -0700

    timestamps

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,12 @@ fs::path getGDPath() {
     return fs::path(CCFileUtils::sharedFileUtils()->getWritablePath());
 }
 
-int getFolderContentLength(fs::path folder) {
-    auto dirIter = fs::directory_iterator(folder);
-    int count = 0;
-    for (auto& p : dirIter) {
-        std::cout << std::to_string(count) << std::endl;
-        std::cout << p.path() << std::endl;
-        if (p.is_directory()) count++;
-    }
-    return count - 1;
-}
-
-void copyFileToBackup(fs::path input, int backupIdx) {
+void copyFileToBackup(fs::path input, std::tm tm) {
     auto gdPath = getGDPath();
-    auto backupPath = gdPath / fs::path("backups") / fs::path("backup" + std::to_string(backupIdx));
+    std::stringstream timestampStr;
+    auto timestamp = std::put_time(&tm, "%Y-%m-%d %Hh%Mm%Ss");
+    timestampStr << timestamp;;
+    auto backupPath = gdPath / fs::path("backups") / fs::path("backup " + timestampStr.str());
     std::cout << backupPath << std::endl;
     fs::create_directories(backupPath);
     fs::copy_file(gdPath / input.filename(), backupPath / input.filename(), fs::copy_options::overwrite_existing);
@@ -35,11 +27,14 @@ void mod_main(HMODULE) {
     // this creates a console window whenever the mod is injected
     // which is very useful for debugging, but make sure to remove
     // on release builds! :D
-    // matdash::create_console();
-    int backupIdx = getFolderContentLength(getGDPath() / fs::path("backups")) + 1;
+    matdash::create_console();
     std::string backupFiles[4] = {"CCLocalLevels.dat", "CCLocalLevels2.dat", "CCGameManager.dat", "CCGameManager2.dat"};
+
+    std::time_t now_tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::tm tm = *std::localtime(&now_tt);
+
     for (auto& f : backupFiles) {
-        copyFileToBackup(f, backupIdx);
+        copyFileToBackup(f, tm);
     }
     // TODO: compress this, handle that later
 }
